af490ad52835d9939787755ecd7f8761d1e7bcaf
kishorereddy <kishorereddy996@gmail.com>
2020-01-28 16:38:33 +0530

    [kishorereddy] Refractor. removed logical programs from logical

--- LogicalPrograms/src/Gambler.zsh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash -x
-source Utility.zsh
-echo "enter the intial amount :"
-# shellcheck disable=SC2162
-read intialamount
-echo "enter the goal amount : "
-read goal
-gambler $intialamount $goal
